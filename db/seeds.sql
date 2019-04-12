66c19b0269102f92b8a80c3eb2a1d547f122aaf0
Oogapooki <andrewosborn101@gmail.com>
2019-04-11 23:39:24 -0500

    added layout

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krabby Patty", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Nasty Patty", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krabby Supreme", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krusty Krab Pizza", false);
